52ddf84f0f81ac6c0c11dddbf703303ddc85e9a8
Matt O'Leary <matto2@users.noreply.github.com>
2024-01-19 19:23:35 -0800

    added download button and file-to-download

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: netlify()
 });
+
+
